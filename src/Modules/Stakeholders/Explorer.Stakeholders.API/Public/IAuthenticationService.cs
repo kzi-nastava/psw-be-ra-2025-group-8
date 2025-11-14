da8ed1cb59394feea09da517835ec2549a3c0ede
ticaos <82711043+ticaos@users.noreply.github.com>
2025-11-14 20:32:40 +0100

    implemented Dto classes and repository

--- src/Modules/Stakeholders/Explorer.Stakeholders.API/Public/IAuthenticationService.cs
+++ src/Modules/Stakeholders/Explorer.Stakeholders.API/Public/IAuthenticationService.cs
@@ -6,4 +6,10 @@ public interface IAuthenticationService
 {
     AuthenticationTokensDto Login(CredentialsDto credentials);
     AuthenticationTokensDto RegisterTourist(AccountRegistrationDto account);
+
+
+    // Admin operations
+    AccountDto CreateAccountByAdmin(AdminCreateAccountDto account);            // create admin/author
+    IEnumerable<AccountOverviewDto> GetAllAccounts();                         // list all users (no password)
+    void SetAccountActiveState(long userId, bool isActive);                   // block/unblock
 }
