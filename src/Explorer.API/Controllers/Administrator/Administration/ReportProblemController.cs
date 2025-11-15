296f412bb3e3fb7135c557a6bdd71c005ea16bde
LukaKeselj <lukakeselj1505@gmail.com>
2025-11-15 14:39:05 +0100

    feat:ReportProblem Class Dto Service IService and some other things were made

---
+++ src/Explorer.API/Controllers/Administrator/Administration/ReportProblemController.cs
@@ -0,0 +1,45 @@
+﻿using Explorer.BuildingBlocks.Core.UseCases;
+using Explorer.Tours.API.Dtos;
+using Explorer.Tours.API.Public.Administration;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Explorer.API.Controllers.Administrator.Administration;
+
+[Authorize(Policy = "administratorPolicy")]
+[Route("api/administration/report-problem")]
+[ApiController]
+public class ReportProblemController : ControllerBase
+{
+    private readonly IReportProblemService _reportProblemService;
+
+    public ReportProblemController(IReportProblemService reportProblemService)
+    {
+        _reportProblemService = reportProblemService;
+    }
+
+    [HttpGet]
+    public ActionResult<PagedResult<ReportProblemDto>> GetAll([FromQuery] int page, [FromQuery] int pageSize)
+    {
+        return Ok(_reportProblemService.GetPaged(page, pageSize));
+    }
+
+    [HttpPost]
+    public ActionResult<ReportProblemDto> Create([FromBody] ReportProblemDto problem)
+    {
+        return Ok(_reportProblemService.Create(problem));
+    }
+
+    [HttpPut("{id:long}")]
+    public ActionResult<ReportProblemDto> Update([FromBody] ReportProblemDto problem)
+    {
+        return Ok(_reportProblemService.Update(problem));
+    }
+
+    [HttpDelete("{id:long}")]
+    public ActionResult Delete(long id)
+    {
+        _reportProblemService.Delete(id);
+        return Ok();
+    }
+}
---
+++ src/Modules/Tours/Explorer.Tours.Core/UseCases/Administration/ReportProblemService.cs
@@ -0,0 +1,49 @@
+﻿using AutoMapper;
+using Explorer.BuildingBlocks.Core.UseCases;
+using Explorer.Tours.API.Dtos;
+using Explorer.Tours.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Tours.Core.UseCases.Administration
+{
+    public class ReportProblemService: IReportProblemService
+    {
+        private readonly ICrudRepository<ReportProblem> _crudRepository;
+        private readonly IMapper _mapper;
+
+        public ReportProblemService(ICrudRepository<ReportProblem> repository, IMapper mapper)
+        {
+            _crudRepository = repository;
+            _mapper = mapper;
+        }
+
+        public PagedResult<ReportProblemDto> GetPaged(int page, int pageSize)
+        {
+            var result = _crudRepository.GetPaged(page, pageSize);
+
+            var items = result.Results.Select(_mapper.Map<ReportProblemDto>).ToList();
+            return new PagedResult<ReportProblemDto>(items, result.TotalCount);
+        }
+
+        public ReportProblemDto Create(ReportProblemDto entity)
+        {
+            var result = _crudRepository.Create(_mapper.Map<ReportProblem>(entity));
+            return _mapper.Map<ReportProblemDto>(result);
+        }
+
+        public ReportProblemDto Update(ReportProblemDto entity)
+        {
+            var result = _crudRepository.Update(_mapper.Map<ReportProblem>(entity));
+            return _mapper.Map<ReportProblemDto>(result);
+        }
+
+        public void Delete(long id)
+        {
+            _crudRepository.Delete(id);
+        }
+    }
+}
