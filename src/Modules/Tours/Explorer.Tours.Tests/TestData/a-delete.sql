2ac1bac341ace920b08d2154b598744f1b932343
mihajlovic189 <marko.mihajlovic189@gmail.com>
2025-11-15 16:35:32 +0100

    final touches

--- src/Modules/Tours/Explorer.Tours.Tests/TestData/a-delete.sql
+++ src/Modules/Tours/Explorer.Tours.Tests/TestData/a-delete.sql
@@ -1,6 +1,2 @@
 ﻿DELETE FROM tours."Equipment";
 DELETE FROM tours."Facilities";
-
--- Reset sequences to start from 1 for new inserts
-ALTER SEQUENCE tours."Equipment_Id_seq" RESTART WITH 1;
-ALTER SEQUENCE tours."Facilities_Id_seq" RESTART WITH 1;
