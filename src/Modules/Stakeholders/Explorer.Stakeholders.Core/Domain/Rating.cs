5c630e18705288a254c3d24e1f96ae3d6e85d067
DavidTot <63733844+DavidTot@users.noreply.github.com>
2025-11-17 16:57:41 +0100

    Defined domain entity Rating

---
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Core/Domain/Rating.cs
@@ -0,0 +1,34 @@
+﻿using Explorer.BuildingBlocks.Core.Domain;
+
+namespace Explorer.Stakeholders.Core.Domain
+{
+    internal class Rating : Entity
+    {
+        public long UserId {  get; init; } 
+        public int Grade { get; private set; }
+        public string? Comment {  get; private set; }
+        public DateTime CreationDate {  get; private set; }
+        public Rating(long userId, int grade, string? comment)
+        {
+            Validate(userId, grade);
+
+            UserId = userId;
+            Grade = grade;
+            Comment = comment;
+            CreationDate = DateTime.Now;
+        }
+
+        private void Validate(long userId, int grade)
+        {
+            if (userId < 0)
+            {
+                throw new ArgumentException("Invalid User ID.");
+            }
+
+            if (grade < 1 || grade > 5)
+            {
+                throw new ArgumentException("Invalid Grade. Grade must be between 1 and 5.");
+            }
+        }
+    }
+}
