5b7d4b7bcec66753dac1f6459506176ce4fcaf64
Zoran <zoranjankovic2003@gmail.com>
2025-11-17 19:55:06 +0100

    fix: moved message feature from Blog module to Stakeholders module

---
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Core/Domain/Message.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Explorer.BuildingBlocks.Core.Domain;
+
+namespace Explorer.Stakeholders.Core.Domain
+{
+    public class Message : Entity
+    {
+        public long SenderId { get; private set; }
+        public long RecipientId { get; private set; }
+        public string Content { get; private set; }
+        public DateTime TimestampCreated { get; private set; }
+        public DateTime? TimestampUpdated { get; private set; }
+        public bool IsDeleted { get; private set; }
+
+        protected Message() { }
+
+        public Message(long senderId, long recipientId, string content)
+        {
+            SenderId = senderId;
+            RecipientId = recipientId;
+            Content = content;
+            TimestampCreated = DateTime.UtcNow;
+            IsDeleted = false;
+        }
+
+        public void Edit(string newContent)
+        {
+            Content = newContent;
+            TimestampUpdated = DateTime.UtcNow;
+        }
+
+        public void Delete()
+        {
+            IsDeleted = true;
+            Content = "Message deleted";
+            TimestampUpdated = DateTime.UtcNow;
+        }
+    }
+}
---
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Core/Domain/RepositoryInterfaces/IMessageRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Explorer.Stakeholders.Core.Domain.RepositoryInterfaces
+{
+    public interface IMessageRepository
+    {
+        Message Create(Message message);
+        Message Get(long id);
+        IEnumerable<Message> GetConversation(long userId1, long userId2);
+        Message Update(Message message);
+    }
+}
