d8eb49117c7e76b589eddf9aeccc2033f7571e7f
Zoran <zoranjankovic2003@gmail.com>
2025-11-17 19:32:29 +0100

    feat: add message dto, service, repository interface and mapper

---
+++ src/Modules/Blog/Explorer.Blog.Core/Mappers/StakeholdersProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Explorer.Stakeholders.API.Dtos;
+using Explorer.Stakeholders.Core.Domain;
+
+namespace Explorer.Stakeholders.Core.Mappers
+{
+    public class StakeholdersProfile : Profile
+    {
+        public StakeholdersProfile()
+        {
+            CreateMap<Message, MessageDto>().ReverseMap();
+        }
+    }
+}
