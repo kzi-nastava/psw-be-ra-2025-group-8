cc94ef3bdd4ba64df27bed8a00afe765e0638994
Rade Todorovic <radetodorovic.2001@gmail.com>
2025-11-18 16:37:00 +0100

    all tests work fine now

--- src/Modules/Stakeholders/Explorer.Stakeholders.Tests/TestData/a-delete-stakeholders.sql
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Tests/TestData/a-delete-stakeholders.sql
@@ -1,5 +1,1 @@
-INSERT INTO stakeholders."Meetups"
-    ("Id", "Name", "Description", "ScheduledAt", "Latitude", "Longitude", "CreatorId")
-VALUES
-    (-1000, 'Test meetup 1', 'Prvi test meetup', '2025-01-01 10:00:00+00', 45.25, 19.83, -11),
-    (-1001, 'Test meetup 2', 'Drugi test meetup', '2025-01-02 11:00:00+00', 45.26, 19.84, -11);
+--kom
