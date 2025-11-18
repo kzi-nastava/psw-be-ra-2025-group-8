6e1fc7dab787850a6a1a93bee2db47e19647ba01
markom03 <minic.ra217.2022@uns.ac.rs>
2025-11-18 12:59:23 +0100

    feat: preferences, quick fix tags and preference tags class

---
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Core/Domain/PreferenceTags.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Explorer.BuildingBlocks.Core.Domain;
+
+namespace Explorer.Stakeholders.Core.Domain
+{
+    public class PreferenceTags : Entity
+    {
+        // ova klasa vezuje tagove za preferencu
+        // jedna preferenca ima 0 ili vise tagova
+
+        public long TouristPreferencesId { get; set; }
+        public long TagsId { get; set; }
+
+        public PreferenceTags() { }
+
+        public PreferenceTags(long tpId, long tId)
+        {
+            TouristPreferencesId = tpId;
+            TagsId = tId;
+        }
+    }
+}
---
+++ src/Modules/Tours/Explorer.Tours.Core/Domain/Tags.cs
@@ -0,0 +1,25 @@
+﻿using Explorer.BuildingBlocks.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Tours.Core.Domain
+{
+    public class Tags : Entity
+    {
+        public string? Tag { get; set; }
+
+        // ovo je klasa koja implementira tagove kad se prave preference u profilu turiste
+        // trebalo bi da se ova klasa dalje koristi kada se npr tura pravi pa tagovi na turi
+
+        public Tags() { } //prazan
+
+        public Tags(string tag) //sa parametrima
+        {
+            Tag = tag;
+        }
+    }
+}
+
