3192bfdefb78a74669901bc1553140d053369e1d
Bringsla <ivradov@gmail.com>
2025-12-10 09:26:53 +0100

    Added dtos

---
+++ src/Modules/Tours/Explorer.Tours.API/Dtos/TouristTourPreviewDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Tours.API.Dtos
+{
+    public class TouristTourPreviewDto
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+
+        public double AverageRating { get; set; }
+
+        public KeyPointPreviewDto FirstKeyPoint { get; set; }
+
+        public List<string> Tags { get; set; }
+        public List<string> RequiredEquipment { get; set; }
+    }
+}
