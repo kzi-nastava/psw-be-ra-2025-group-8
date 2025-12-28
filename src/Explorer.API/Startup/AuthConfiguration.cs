b85459276589f4ac8814d661a541afc76ff13d12
Lazar Nestorovic <lazarnestorovvic003@gmail.com>
2025-12-28 17:07:55 +0100

    Implemented SignalR WebSocet

--- src/Explorer.API/Startup/AuthConfiguration.cs
+++ src/Explorer.API/Startup/AuthConfiguration.cs
@@ -43,6 +43,18 @@ public static class AuthConfiguration
                         }
 
                         return Task.CompletedTask;
+                    },
+                    OnMessageReceived = context =>
+                    {
+                        var accessToken = context.Request.Query["access_token"];
+                        var path = context.HttpContext.Request.Path;
+                        
+                        if (!string.IsNullOrEmpty(accessToken) && path.StartsWithSegments("/hubs"))
+                        {
+                            context.Token = accessToken;
+                        }
+
+                        return Task.CompletedTask;
                     }
                 };
             });
