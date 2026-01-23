729e1c564794d9069305172c6632be58d7a36cc2
mihajlovic189 <marko.mihajlovic189@gmail.com>
2026-01-23 10:56:35 +0100

    feat: Added source enum for Position

--- src/Modules/Tours/Explorer.Tours.Core/Domain/Position.cs
+++ src/Modules/Tours/Explorer.Tours.Core/Domain/Position.cs
@@ -1,39 +1,47 @@
 ﻿using Explorer.BuildingBlocks.Core.Domain;
 
-namespace Explorer.Tours.Core.Domain;
-
-public class Position : Entity
-{
-    public double Latitude { get; set; }
-    public double Longitude { get; set; }
-    public int TouristId { get; set; }
-    public DateTime UpdatedAt { get; set; }
-
-    public Position(double latitude, double longitude, int touristId)
+namespace Explorer.Tours.Core.Domain
+{ 
+    public class Position : Entity
     {
-        Latitude = latitude;
-        Longitude = longitude;
-        TouristId = touristId;
-        UpdatedAt = DateTime.UtcNow;
-        Validate();
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public int TouristId { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public Source LocationSource { get; set; }
+
+        public Position(double latitude, double longitude, int touristId, Source locationSource)
+        {
+            Latitude = latitude;
+            Longitude = longitude;
+            TouristId = touristId;
+            UpdatedAt = DateTime.UtcNow;
+            LocationSource = locationSource;
+            Validate();
+        }
+
+        public Position() { }
+
+        public void UpdatePosition(double latitude, double longitude)
+        {
+            Latitude = latitude;
+            Longitude = longitude;
+            UpdatedAt = DateTime.UtcNow;
+            Validate();
+        }
+
+        private void Validate()
+        {
+            if (Latitude < -90 || Latitude > 90)
+                throw new ArgumentException("Latitude must be between -90 and 90");
+
+            if (Longitude < -180 || Longitude > 180)
+                throw new ArgumentException("Longitude must be between -180 and 180");
+        }
     }
-
-    public Position() { }
-
-    public void UpdatePosition(double latitude, double longitude)
+    public enum Source
     {
-        Latitude = latitude;
-        Longitude = longitude;
-        UpdatedAt = DateTime.UtcNow;
-        Validate();
-    }
-
-    private void Validate()
-    {
-        if (Latitude < -90 || Latitude > 90)
-            throw new ArgumentException("Latitude must be between -90 and 90");
-
-        if (Longitude < -180 || Longitude > 180)
-            throw new ArgumentException("Longitude must be between -180 and 180");
+        Automatic,
+        Manual
     }
 }
