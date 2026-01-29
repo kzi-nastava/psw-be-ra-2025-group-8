2c9d53939f59ddf1e3a94900e76faaccf0f5cc3f
DavidTot <63733844+DavidTot@users.noreply.github.com>
2026-01-29 04:27:25 +0100

    feat: Added logic to make economics graph and expanded sql for testing

---
+++ src/Modules/Payments/Explorer.Payments.API/Explorer.Payments.API/Dtos/TourEconomicStatisticsDto.cs
@@ -0,0 +1,9 @@
+﻿public class TourEconomicStatisticsDto
+{
+    public DateTime TimePeriod { get; set; }
+    public decimal TotalRevenue { get; set; }
+    public int SalesCount { get; set; }
+    public decimal RevenueFromCoupons { get; set; }
+    public decimal RevenueFromSales { get; set; }
+    public decimal RevenueFullPrice { get; set; }
+}
